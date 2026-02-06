da694ee181a8b7c37b6ac275085437c01e6eb2c3
Martí Bosch <marti.bosch@protonmail.com>
2026-02-06 10:53:02 +0100

    feat!: surface3d_gdf and dst_dir kwargs in get_tree_canopy_raster

--- tests/test_swisstopopy.py
+++ tests/test_swisstopopy.py
@@ -1,5 +1,6 @@
 """Tests for swisstopopy."""
 
+import glob
 import importlib
 import logging as lg
 import sys
@@ -103,29 +104,58 @@ class TestSwissTopoPy(unittest.TestCase):
             # test that we do NOT get a "height" column
             self.assertNotIn("height", bldg_gdf.columns)
 
-    def _test_raster(self, func, kwargs_combinations):
+    def _test_dst_raster(self, dst_filepath):
+        # test that we get a non empty image-like raster
+        with rio.open(dst_filepath) as src:
+            self.assertEqual(src.count, 1)
+            self.assertEqual(len(src.read(1).shape), 2)
+
+    def _test_raster(self, func, kwargs_combinations, *, keyword_only=False):
+        # define call_func here to avoid checking keyword_only in every iteration.
+        if keyword_only:
+
+            def call_func(dst_filepath, **kwargs):
+                # note that in this case we get the region from the kwargs first in case
+                # it has been provided explicitly
+                if "region" in kwargs:
+                    region = kwargs.pop("region")
+                else:
+                    region = self.region
+                return func(
+                    region=region,
+                    dst_filepath=dst_filepath,
+                    **kwargs,
+                )
+        else:
+
+            def call_func(dst_filepath, **kwargs):
+                return func(self.region, dst_filepath, **kwargs)
+
+        # test all combinations of kwargs
         for kwargs in kwargs_combinations:
             with tempfile.TemporaryDirectory() as tmp_dir:
                 dst_filepath = path.join(tmp_dir, "foo.tif")
-                func(
-                    self.region,
-                    dst_filepath,
-                    **kwargs,
-                )
+                # call the function
+                call_func(dst_filepath, **kwargs)
                 # test that we get a non empty raster
-                with rio.open(dst_filepath) as src:
-                    self.assertEqual(src.count, 1)
-                    self.assertEqual(len(src.read(1).shape), 2)
+                self._test_dst_raster(dst_filepath)
 
-    def _test_wrong_datetime(self, func, kwargs):
+    def _test_wrong_datetime(self, func, kwargs, *, keyword_only=False):
         with tempfile.TemporaryDirectory() as tmp_dir:
             dst_filepath = path.join(tmp_dir, "foo.tif")
             with self.assertRaises(ValueError):
-                func(
-                    self.region,
-                    dst_filepath,
-                    **kwargs,
-                )
+                if keyword_only:
+                    func(
+                        region=self.region,
+                        dst_filepath=dst_filepath,
+                        **kwargs,
+                    )
+                else:
+                    func(
+                        self.region,
+                        dst_filepath,
+                        **kwargs,
+                    )
 
     def test_dem(self):
         self._test_raster(
@@ -149,23 +179,77 @@ class TestSwissTopoPy(unittest.TestCase):
                 "cache_lidar": True,
                 "pooch_retrieve_kwargs": {"path": tmp_dir},
             }
+            # get example surface3d_gdf
+            client = swisstopopy.SwissTopoClient(region=self.region)
+            surface3d_gdf = client.get_collection_gdf(
+                swisstopopy.SWISSSURFACE3D_COLLECTION_ID,
+            )
+            surface3d_gdf = surface3d_gdf[
+                surface3d_gdf["assets.href"].str.endswith(".zip")
+            ]
+            surface3d_gdf = swisstopopy.get_latest(surface3d_gdf)
+            # note that _test_raster only works for dst_filepath, so we will test
+            # passing dst_dir separately below
+            # also note that the default "None" value for region will default to the
+            # region from the test class, so we don't need to specify it in the kwargs
+            # combinations except when we set it to None to explicitly test passing
+            # None (to use surface3d_gdf instead of region)
             self._test_raster(
                 swisstopopy.get_tree_canopy_raster,
                 [
                     {**kwargs, **_base_kwargs}
                     for kwargs in [
                         {},
+                        {"region": None, "surface3d_gdf": surface3d_gdf},
                         {"surface3d_datetime": "2019"},
                         {"count_threshold": 48},
                         {"dst_res": 2},
                         {"dst_tree_val": 255, "dst_dtype": "int16", "dst_nodata": -255},
                     ]
                 ],
+                keyword_only=True,
             )
+            # test wrong datetime
             self._test_wrong_datetime(
                 swisstopopy.get_tree_canopy_raster,
                 {"surface3d_datetime": "2018"},
+                keyword_only=True,
             )
+            # output filepath for the following tests
+            # test that when providing both a dir a filepath, the filepath takes
+            # precedence
+            ignored_dir = path.join(tmp_dir, "ignored-output")
+            dst_filepath = path.join(tmp_dir, "foo.tif")
+            swisstopopy.get_tree_canopy_raster(
+                region=self.region,
+                dst_filepath=dst_filepath,
+                dst_dir=ignored_dir,
+                **_base_kwargs,
+            )
+            self.assertFalse(path.exists(ignored_dir))
+            self._test_dst_raster(dst_filepath)
+            # test providing dst_dir only
+            dst_dir = path.join(tmp_dir, "tree-canopy")
+            swisstopopy.get_tree_canopy_raster(
+                region=self.region,
+                dst_dir=dst_dir,
+                **_base_kwargs,
+            )
+            # test that dst_dir exists
+            self.assertTrue(path.exists(dst_dir))
+            # test that dst_dir contains valid tif files
+            tif_filepaths = glob.glob(path.join(dst_dir, "*.tif"))
+            self.assertGreater(
+                len(tif_filepaths), 0, "Expected at least one .tif file in dst_dir."
+            )
+            for tif_filepath in tif_filepaths:
+                # test that tif_filepath is a file
+                self.assertTrue(
+                    path.isfile(tif_filepath),
+                    f"Expected {tif_filepath} to be a file.",
+                )
+                # test that tif_filepath is a valid raster
+                self._test_dst_raster(tif_filepath)
 
     @pytest.mark.usefixtures("unload_pdal")
     def test_tree_canopy_no_pdal(self):
@@ -175,8 +259,8 @@ class TestSwissTopoPy(unittest.TestCase):
             dst_filepath = path.join(tmp_dir, "foo.tif")
             with self.assertLogs(level=lg.WARNING) as cm:
                 result = swisstopopy.get_tree_canopy_raster(
-                    self.region,
-                    dst_filepath,
+                    region=self.region,
+                    dst_filepath=dst_filepath,
                 )
             self.assertIsNone(result)
             self.assertTrue(
